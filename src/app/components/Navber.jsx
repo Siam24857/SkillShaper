1d4067c4d2d4af9c4707066bdaebbe33c507d407
Siam24857 <siamtechofficial1597@gmail.com>
2026-04-29 22:27:28 +0600

    this is my first commmite

---
+++ src/app/components/Navber.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Navber = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      
+    </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>About 1</a></li>
+       
+      <li><a>Carear</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end flex gap-3">
+     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+    <a className="btn">Button</a>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Navber;
